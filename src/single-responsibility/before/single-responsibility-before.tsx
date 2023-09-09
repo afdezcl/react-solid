cdc072c27de16490aaeb3fd809f119de435ee54b
Adrian Fernandez <adrian.fernandez@zinklar.com>
2023-09-09 12:35:32 +0200

    add example code to single responsibility

--- src/single-responsibility/before/single-responsibility-before.tsx
+++ src/single-responsibility/before/single-responsibility-before.tsx
@@ -1,7 +1,35 @@
+import { useState, useEffect } from 'react'
+
+interface Post {
+  id: number
+  title: string
+  content: string
+  publishedAt: Date
+}
+
 export function SingleResponsibilityBefore() {
+  const [posts, setPosts] = useState<Post[]>([])
+  
+  useEffect(() => {
+    const loadPosts = async () => {  
+      const response = await fetch('/some-api')
+      const data = await response.json()
+      setPosts(data)
+    }
+    loadPosts()
+  }, [])
+  
+  const weekAgo = new Date();
+  weekAgo.setDate(weekAgo.getDate() - 7);
+
   return (
-    <div>
-      <h1>Single Responsibility Principle Before</h1>
-    </div>
-  );
+    <ul>
+      {posts.filter(post => post.publishedAt >= weekAgo).map(post => 
+        <li key={post.id}>
+          <h1>{post.title}</h1>
+          <p>{post.content}</p>
+        </li>
+      )}
+    </ul>    
+  )
 }
